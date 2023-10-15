cb8cff52762a64f8d61337fe018583d5b94e3d1a
geunheejung <geunhee0212@gmail.com>
2023-10-15 15:34:20 +0900

    chore: Setup react project env.

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import React from 'react';
+
+const container = document.getElementById('app') as HTMLElement;
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+
